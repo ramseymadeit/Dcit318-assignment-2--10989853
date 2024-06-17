4ccd8f6d59a3bd3b6738cf4240950419768b6fbb
ramseymadeit <103139610+ramseymadeit@users.noreply.github.com>
2024-06-17 12:38:27 -0700

    Add files via upload

---
+++ Animal Sound ConsoleApp1/Animal Sound ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Animal_Sound_ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Sound_ConsoleApp1
+{
+   
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+}
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
+
+
+
