5b8e2f6bf69caae6f34c1ea2f9d33d4b85a1e49a
ramseymadeit <103139610+ramseymadeit@users.noreply.github.com>
2024-06-17 12:53:06 -0700

    Add files via upload

---
+++ IMovable ConsoleApp1/IMovable ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMovable_ConsoleApp1
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            myCar.Move();
+            myBicycle.Move();
+        }
+    }
+
+}
