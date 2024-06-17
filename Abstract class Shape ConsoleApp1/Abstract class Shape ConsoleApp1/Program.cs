042181e965dc1f7bdb2a817e79296e6e2edab9b9
ramseymadeit <103139610+ramseymadeit@users.noreply.github.com>
2024-06-17 12:48:42 -0700

    Add files via upload

---
+++ Abstract class Shape ConsoleApp1/Abstract class Shape ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using Abstract_class_Shape_ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_Shape_ConsoleApp1
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape myCircle = new Circle(5);
+        Shape myRectangle = new Rectangle(4, 6);
+
+        Console.WriteLine("Area of Circle: " + myCircle.GetArea());
+        Console.WriteLine("Area of Rectangle: " + myRectangle.GetArea());
+    }
+}
